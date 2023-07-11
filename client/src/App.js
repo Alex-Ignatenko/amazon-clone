9d9dbfc28d08e4b2bfcf61534fe2d0e65c9d7e47
Alex Ignatenko <alex80850@gmail.com>
2023-07-11 22:28:23 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import NavBar from "./components/navBar";
+import HomePage from "./pages/homePage";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
